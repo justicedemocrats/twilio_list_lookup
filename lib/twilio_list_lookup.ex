3decf5e1a5a4430466f0a55bd778f00210d65836
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-10-19 16:44:11 -0700

    run on bsd or van exports

--- lib/twilio_list_lookup.ex
+++ lib/twilio_list_lookup.ex
@@ -5,27 +5,23 @@ defmodule TwilioListLookup do
   import Ecto.Query
 
   def lookup_and_partition_list(filename) do
-    # [main_output, landline_output, mobile_output, voip_output, unknown_output, not_found_output] =
-        # ~w( -processed -landline -mobile -voip -unknown -not-found),
+    {:ok, file} = File.open(filename, [:read])
+    key_line = IO.read(file, :line)
+    File.close(file)
+
+    extractor = key_line |> String.trim() |> gen_extractor()
+
     [main_output, landline_output, other_output] =
       Enum.map(
         ~w(-processed -landline -other),
         fn appendage -> File.open!(create_path(filename, appendage), [:write]) end)
 
-    IO.binwrite(main_output, "Account,Voter File VANID,FirstName,LastName,Fullname,Phone1,Phone2,vAddress,Adress2,City,State,Zip5,Age,Sex,Party,Type\n")
-    IO.binwrite(landline_output, "Account,Voter File VANID,FirstName,LastName,Fullname,Phone1,Phone2,vAddress,Adress2,City,State,Zip5,Age,Sex,Party\n")
-    IO.binwrite(other_output, "Account,Voter File VANID,FirstName,LastName,Fullname,Phone1,Phone2,vAddress,Adress2,City,State,Zip5,Age,Sex,Party\n")
-    # IO.binwrite(mobile_output, "Account,Voter File VANID,FirstName,LastName,Fullname,Phone1,Phone2,vAddress,Adress2,City,State,Zip5,Age,Sex,Party\n")
-    # IO.binwrite(voip_output, "Account,Voter File VANID,FirstName,LastName,Fullname,Phone1,Phone2,vAddress,Adress2,City,State,Zip5,Age,Sex,Party\n")
-    # IO.binwrite(unknown_output, "Account,Voter File VANID,FirstName,LastName,Fullname,Phone1,Phone2,vAddress,Adress2,City,State,Zip5,Age,Sex,Party\n")
-    # IO.binwrite(not_found_output, "Account,Voter File VANID,FirstName,LastName,Fullname,Phone1,Phone2,vAddress,Adress2,City,State,Zip5,Age,Sex,Party\n")
+    IO.binwrite(main_output, key_line <> "Type\n")
+    IO.binwrite(landline_output, key_line <> "\n")
+    IO.binwrite(other_output, key_line <> "\n")
 
     main_write_fn = fn type, list ->
-      csl =
-        list
-        |> Enum.concat([type])
-        |> Enum.join(",")
-
+      csl = list |> Enum.concat([type]) |> Enum.join(",")
       IO.binwrite(main_output, "#{csl}\n")
     end
 
@@ -35,10 +31,7 @@ defmodule TwilioListLookup do
       output =
         case type do
           "landline" -> landline_output
-          "mobile" -> other_output
-          "voip" -> other_output
-          "unknown" -> other_output
-          "not found" -> other_output
+          _ -> other_output
         end
 
       IO.binwrite(output, "#{csl}\n")
@@ -49,20 +42,16 @@ defmodule TwilioListLookup do
     |> CSV.parse_stream()
     |> Stream.with_index()
     |> Flow.from_enumerable([max_demand: 20, min_demand: 10])
-    |> Flow.map(fn {list, idx} -> {process_line(list, main_write_fn, alt_write_fn), idx} end)
+    |> Flow.map(fn {list, idx} -> {process_line(list, extractor, main_write_fn, alt_write_fn), idx} end)
     |> Flow.map(&report/1)
     |> Flow.run()
 
     Logger.info "Performed #{Counter.get()} twilio lookups"
-
-    Enum.map(
-      # [main_output, landline_output, mobile_output, voip_output],
-      [main_output, landline_output, other_output],
-      fn file -> File.close(file) end)
+    Enum.map([main_output, landline_output, other_output], fn file -> File.close(file) end)
   end
 
-  defp process_line(list, main_write_fn, alt_write_fn) do
-    pn = list |> Enum.at(5) |> get_existing_record()
+  defp process_line(list, extractor, main_write_fn, alt_write_fn) do
+    pn = list |> extractor.() |> get_existing_record()
 
     type =
       case get_lookup(pn) do
@@ -70,6 +59,8 @@ defmodule TwilioListLookup do
         %{carrier: %{"type" => type}} -> type
         %{"error" => _error_message} -> "not found"
         %{error: _error_message} -> "not found"
+        %{carrier: nil} -> "not found"
+        %{"carrier" => nil} -> "not found"
       end
 
     main_write_fn.(type, list)
@@ -79,8 +70,8 @@ defmodule TwilioListLookup do
   end
 
   defp create_path(basefile, appendage) do
-    [base, ending] = String.split(basefile, ".")
-    new_sections = [base, appendage, "." <> ending]
+    [ending | reverse_file_parts] = basefile |> String.split(".") |> Enum.reverse()
+    new_sections = reverse_file_parts |> Enum.reverse() |> Enum.concat([appendage <> "." <> ending])
     Enum.join(new_sections, "")
   end
 
@@ -113,7 +104,7 @@ defmodule TwilioListLookup do
       if pn.id do
         Repo.update!(changeset)
       else
-        case Repo.insert(changeset) do
+        case Repo.insert(changeset, [on_conflict: [set: [twilio_lookup_result: lookup]], conflict_target: :number]) do
           {:ok, inserted} -> inserted
           {:error, _error} -> Repo.update((from pn in PhoneNumber, where: pn.number == ^number), set: [twilio_lookup_result: lookup])
         end
@@ -124,5 +115,29 @@ defmodule TwilioListLookup do
 
   defp get_lookup(_pn = %PhoneNumber{twilio_lookup_result: lookup}) do
     lookup
+  end
+
+  # VAN
+  defp gen_extractor("Account,Voter File VANID,FirstName,LastName,Fullname,Phone1,Phone2,vAddress,Adress2,City,State,Zip,Age,Sex,Party") do
+    fn list ->
+      case Enum.at(list, 5) do
+        "" -> Enum.at(list, 6)
+        num -> num
+      end
+    end
+  end
+
+  # BSD Export
+  defp gen_extractor("Unique Constituent ID,Prefix,First Name,Middle Name,Last Name,Suffix,Gender,Address 1,Address 2,City,State,Zip,County,Congressional District,State House District,State Senate District,Country,Employer,Occupation,Email,Home Phone,Work Phone,Fax,Mobile Phone,Mobile Phone Opt-In,Join Date,Source,Subsource") do
+    fn list ->
+      case Enum.at(list, 20) do
+        "" ->
+          case Enum.at(list, 21) do
+            "" -> Enum.at(list, 23) # mobile numb
+            work_numb -> work_numb
+          end
+        home_numb -> home_numb
+      end
+    end
   end
 end
