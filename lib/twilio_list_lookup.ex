1d13a0e53645453ffd03569823734acff4cb0809
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-10-23 10:06:28 -0700

    export mobiles independently

--- lib/twilio_list_lookup.ex
+++ lib/twilio_list_lookup.ex
@@ -11,26 +11,28 @@ defmodule TwilioListLookup do
 
     extractor = key_line |> String.trim() |> gen_extractor()
 
-    [main_output, landline_output, other_output] =
+    [main_output, landline_output, mobile_output, other_output] =
       Enum.map(
-        ~w(-processed -landline -other),
+        ~w(-processed -landline -mobile -other),
         fn appendage -> File.open!(create_path(filename, appendage), [:write]) end)
 
     IO.binwrite(main_output, key_line <> "Type\n")
     IO.binwrite(landline_output, key_line <> "\n")
+    IO.binwrite(mobile_output, key_line <> "\n")
     IO.binwrite(other_output, key_line <> "\n")
 
     main_write_fn = fn type, list ->
-      csl = list |> Enum.concat([type]) |> Enum.join(",")
+      csl = list |> Enum.concat([type]) |> Enum.map(& ~s("#{&1}")) |> Enum.join(",")
       IO.binwrite(main_output, "#{csl}\n")
     end
 
     alt_write_fn = fn type, list ->
-      csl = Enum.join list, ","
+      csl = list |> Enum.map(& ~s("#{&1}")) |> Enum.join(",")
 
       output =
         case type do
           "landline" -> landline_output
+          "mobile" -> mobile_output
           _ -> other_output
         end
 
@@ -47,7 +49,7 @@ defmodule TwilioListLookup do
     |> Flow.run()
 
     Logger.info "Performed #{Counter.get()} twilio lookups"
-    Enum.map([main_output, landline_output, other_output], fn file -> File.close(file) end)
+    Enum.map([main_output, landline_output, mobile_output, other_output], fn file -> File.close(file) end)
   end
 
   defp process_line(list, extractor, main_write_fn, alt_write_fn) do
@@ -120,24 +122,32 @@ defmodule TwilioListLookup do
   # VAN
   defp gen_extractor("Account,Voter File VANID,FirstName,LastName,Fullname,Phone1,Phone2,vAddress,Adress2,City,State,Zip,Age,Sex,Party") do
     fn list ->
-      case Enum.at(list, 5) do
-        "" -> Enum.at(list, 6)
-        num -> num
-      end
+      get_num(list, [5, 6])
+    end
+  end
+
+  # Alternative VAN
+  defp gen_extractor("Voter File VANID,Sex,LastName,FirstName,MiddleName,Suffix,Preferred Phone,Cell Phone,Home Phone,Party,Age,WorkPhone, WorkPhoneExt,StreetNo,StreetNoHalf,StreetPrefix,StreetName,StreetType,StreetSuffix,AptType,AptNo,City,State,Zip5,Zip4") do
+    fn list ->
+      get_num(list, [6, 7, 8, 11])
     end
   end
 
   # BSD Export
   defp gen_extractor("Unique Constituent ID,Prefix,First Name,Middle Name,Last Name,Suffix,Gender,Address 1,Address 2,City,State,Zip,County,Congressional District,State House District,State Senate District,Country,Employer,Occupation,Email,Home Phone,Work Phone,Fax,Mobile Phone,Mobile Phone Opt-In,Join Date,Source,Subsource") do
     fn list ->
-      case Enum.at(list, 20) do
-        "" ->
-          case Enum.at(list, 21) do
-            "" -> Enum.at(list, 23) # mobile numb
-            work_numb -> work_numb
-          end
-        home_numb -> home_numb
-      end
+      get_num(list, [20, 21, 23])
+    end
+  end
+
+  defp get_num(list, [curr | rest]) do
+    case Enum.at(list, curr) do
+      "" ->
+        case rest do
+          [] -> ""
+          rest -> get_num(list, rest)
+        end
+      num -> num
     end
   end
 end
