557d90ad1ee2fe6689568d72d4f27e0cf9cc4abb
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-11-29 14:27:41 -0600

    more descriptive error message

--- lib/mix/task.ex
+++ lib/mix/task.ex
@@ -6,8 +6,6 @@ defmodule Mix.Tasks.RunLookup do
     {:ok, _started} = Application.ensure_all_started(:osdi)
     {:ok, _started} = Counter.start_link
 
-    [filename | rest] = args
-
     case args do
       [filename | rest] ->
         TwilioListLookup.lookup_and_partition_list(filename, rest)
